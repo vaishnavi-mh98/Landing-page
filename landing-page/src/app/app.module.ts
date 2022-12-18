caa53cf0da1d078478f1e7aded5a0ecd3af1a9e9
vaishnavi-mh98 <120893918+vaishnavi-mh98@users.noreply.github.com>
2022-12-17 23:31:47 -0700

    first commit

---
+++ landing-page/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page/landing-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
